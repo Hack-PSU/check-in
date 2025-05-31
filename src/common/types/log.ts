2ab49b6aac9468192e4fe598145d635b4d794291
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2025-05-30 22:28:22 -0400

    quick little log page

--- src/common/types/log.ts
+++ src/common/types/log.ts
@@ -1,10 +1,10 @@
 export interface CheckInLogEntry {
-  scanId: string;
-  userEmail: string;
-  userName: string;
-  eventName: string;
-  organizerEmail: string;
-  organizerName: string;
-  timestamp: number;
-  hackathonId?: string;
+	scanId: string;
+	userEmail: string;
+	userName: string;
+	eventName: string;
+	organizerEmail: string;
+	organizerName: string;
+	timestamp: number;
+	hackathonId?: string;
 }

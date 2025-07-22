567b387e1ef48e256eaade2045276f2d650bfb51
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-07-22 16:38:23 +0530

    opt in

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -6,4 +6,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
 	defaults: "2025-05-24",
 	capture_exceptions: true,
 	debug: process.env.NODE_ENV === "development",
+	opt_in_site_apps: true
 });

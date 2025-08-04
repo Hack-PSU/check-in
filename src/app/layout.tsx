4f75711e34556a0015a8aeb34e55dc9174b3dd3a
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-08-04 12:02:14 +0530

    remove install prompt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,6 @@ export default function RootLayout({
 				<LayoutProvider>
 					{children}
 					<BottomNav />
-					<PWAInstallPrompt />
 					<Toaster richColors position="bottom-right" />
 				</LayoutProvider>
 				<Analytics />

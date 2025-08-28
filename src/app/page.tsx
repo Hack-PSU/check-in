008d550926a525e76e0bc72a01ff83af0d73a20b
Jeremiah Ondrasik <technologyfrog89@gmail.com>
2025-08-28 08:45:15 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Page(): JSX.Element {
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    router.replace("/schedule"); // replace so user can't "go back" to empty page
-  }, [router]);
+	useEffect(() => {
+		router.replace("/schedule"); // replace so user can't "go back" to empty page
+	}, [router]);
 
-  return <></>;
+	return <></>;
 }
